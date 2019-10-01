3a956df41fdbc1f98c3cb35d62c4d85b06cc1ecc
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-01 15:35:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,10 @@
 require "pry"
-# def reverse_each_word(string)
-#   array = string.split(" ")
-#   array.each { |word| word.reverse!}
-#   array.join(" ")
-# end
+def reverse_each_word(string)
+  new_array = []
+  array = string.split(" ")
+  array.each do |word| 
+    word.reverse!
+  
+  end
+  array.join(" ")
+end
